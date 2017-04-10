510b450bb688fe715c91059fdc0b26cd100e24d9
Yevhen Kryvun <yevhen.kryvun@cloudlock.com>
2017-04-10 13:56:27 +0300

    Added changes

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home_page(request):
-  return HttpResponse('<html><title>To-Do lists</title></html>')
+  if request.method == 'POST':
+    return HttpResponse(request.POST['item_text'])
+  return render(request, 'home.html')
